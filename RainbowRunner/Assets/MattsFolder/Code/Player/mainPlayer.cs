ef7bea3e87fb235f1ee7947067d8f3ceb8c6ca1f
Matthew Wilber <m.wilber98@gmail.com>
2013-10-26 13:21:28 -0400

    Updates to player

--- RainbowRunner/Assets/MattsFolder/Code/Player/mainPlayer.cs
+++ RainbowRunner/Assets/MattsFolder/Code/Player/mainPlayer.cs
@@ -3,11 +3,16 @@ using System.Collections;
 
 public class mainPlayer : MonoBehaviour {
 	
-	//Speed
-	float offColorSpeed = 0.0f;
-	float onColorSpeed = 0.0f;
-	float currentSpeed = 0.0f;
+	//Lane Color Enum
+	enum laneColor {red,purple,blue,cyan,green,yellow};
 	
+	//Current Lane
+	int currentLane = (int)laneColor.red;
+	
+	//Current postion and Scale
+	int yPostion = 0;
+	int xPostion = 0;
+	float playerScale = 0.0f;
 	
 	// Use this for initialization
 	void Start () 
@@ -18,54 +23,89 @@ public class mainPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		//Movement left
+		if(Input.GetKeyDown(KeyCode.D) && currentLane != laneColor.red)
+		{
+			//Move player Down
+			yPostion = yPostion - 0;
+			//Scale player up
+			playerScale = playerScale + 0.0;
+			//change current lane
+			currentLane = currentLane - 1;
+		}
+		//Movement Right
+		if(Input.GetButtonDown(KeyCode.A) && currentLane != laneColor.yellow)
+		{
+			//Move player up
+			yPostion = yPostion + 0;
+			//Scale player down
+			playerScale = playerScale - 0.0;
+			//change current lane
+			currentLane = currentLane + 1;	
+		}
+		//Draw Player
 	}
 	
 	public void OnCollisionEnter(Collision other)
     {
+		#region object collison
 		//Mean Cloud
         if (other.gameObject.tag == "")
         {
 			//Reduce Time
-			
-			//Reset Multiply
-
-			
+			//Reset Multiply		
 			//Add Stumbles
-
         }
-		
 		//Gold Coin
 		if (other.gameObject.tag == "")
         {
 			//Add Coin
-			
         }
-		
 		//Empty Kettle
 		if (other.gameObject.tag == "")
         {
 			//
-			
 			//Reset Multiply
-			
 			//Add Stumbles
-            
         }
-		
 		//Add Time
 		if (other.gameObject.tag == "")
         {
 			//Add Time
-			
-            
-        }
-		
+			AddTime(10);
+		}
 		//Multiplyer up
 		if (other.gameObject.tag == "")
         {
             //Up Multiplyer
+        }
+		#endregion
+		
+		#region lane collision
+		if (other.gameObject.tag == "")
+        {
 
         }
+		if (other.gameObject.tag == "")
+        {
+
+        }
+		if (other.gameObject.tag == "")
+        {
+
+        }
+		if (other.gameObject.tag == "")
+        {
+
+        }
+		if (other.gameObject.tag == "")
+        {
+
+        }
+		if (other.gameObject.tag == "")
+        {
+
+        }
+		#endregion
     }
 }
