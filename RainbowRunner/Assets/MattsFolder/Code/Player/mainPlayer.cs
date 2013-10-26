41faa06ba19781e0d5a3347b6896953cd2cbc9d3
Matthew Wilber <m.wilber98@gmail.com>
2013-10-26 13:35:47 -0400

    made it so pat can so things

--- RainbowRunner/Assets/MattsFolder/Code/Player/mainPlayer.cs
+++ RainbowRunner/Assets/MattsFolder/Code/Player/mainPlayer.cs
@@ -7,7 +7,7 @@ public class mainPlayer : MonoBehaviour {
 	enum laneColor {red,purple,blue,cyan,green,yellow};
 	
 	//Current Lane
-	int currentLane = (int)laneColor.red;
+	int currentLane = 0;
 	
 	//Current postion and Scale
 	int yPostion = 0;
@@ -23,89 +23,89 @@ public class mainPlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		//Movement left
-		if(Input.GetKeyDown(KeyCode.D) && currentLane != laneColor.red)
-		{
-			//Move player Down
-			yPostion = yPostion - 0;
-			//Scale player up
-			playerScale = playerScale + 0.0;
-			//change current lane
-			currentLane = currentLane - 1;
-		}
+//		//Movement left
+//		if(Input.GetKeyDown(KeyCode.D) && currentLane != laneColor.red)
+//		{
+//			//Move player Down
+//			yPostion = yPostion - 0;
+//			//Scale player up
+//			playerScale = playerScale + 0.0;
+//			//change current lane
+//			currentLane = currentLane - 1;
+//		}
 		//Movement Right
-		if(Input.GetButtonDown(KeyCode.A) && currentLane != laneColor.yellow)
-		{
+		//if(Input.GetButtonDown(KeyCode.A) && currentLane != laneColor.yellow)
+		//{
 			//Move player up
-			yPostion = yPostion + 0;
+			//yPostion = yPostion + 0;
 			//Scale player down
-			playerScale = playerScale - 0.0;
+			//playerScale = playerScale - 0.0;
 			//change current lane
-			currentLane = currentLane + 1;	
-		}
+			//currentLane = currentLane + 1;	
+		//}
 		//Draw Player
 	}
 	
 	public void OnCollisionEnter(Collision other)
     {
 		#region object collison
-		//Mean Cloud
-        if (other.gameObject.tag == "")
-        {
-			//Reduce Time
-			//Reset Multiply		
-			//Add Stumbles
-        }
-		//Gold Coin
-		if (other.gameObject.tag == "")
-        {
-			//Add Coin
-        }
-		//Empty Kettle
-		if (other.gameObject.tag == "")
-        {
-			//
-			//Reset Multiply
-			//Add Stumbles
-        }
-		//Add Time
-		if (other.gameObject.tag == "")
-        {
-			//Add Time
-			AddTime(10);
-		}
-		//Multiplyer up
-		if (other.gameObject.tag == "")
-        {
-            //Up Multiplyer
-        }
+//		//Mean Cloud
+//        if (other.gameObject.tag == "")
+//        {
+//			//Reduce Time
+//			//Reset Multiply		
+//			//Add Stumbles
+//        }
+//		//Gold Coin
+//		if (other.gameObject.tag == "")
+//        {
+//			//Add Coin
+//        }
+//		//Empty Kettle
+//		if (other.gameObject.tag == "")
+//        {
+//			//
+//			//Reset Multiply
+//			//Add Stumbles
+//        }
+//		//Add Time
+//		if (other.gameObject.tag == "")
+//        {
+//			//Add Time
+//			AddTime(10);
+//		}
+//		//Multiplyer up
+//		if (other.gameObject.tag == "")
+//        {
+//            //Up Multiplyer
+//        }
 		#endregion
 		
-		#region lane collision
-		if (other.gameObject.tag == "")
-        {
-
-        }
-		if (other.gameObject.tag == "")
-        {
-
-        }
-		if (other.gameObject.tag == "")
-        {
-
-        }
-		if (other.gameObject.tag == "")
-        {
-
-        }
-		if (other.gameObject.tag == "")
-        {
-
-        }
-		if (other.gameObject.tag == "")
-        {
-
-        }
+	#region lane collision
+//		if (other.gameObject.tag == "")
+//        {
+//
+//        }
+//		if (other.gameObject.tag == "")
+//        {
+//
+//        }
+//		if (other.gameObject.tag == "")
+//        {
+//
+//        }
+//		if (other.gameObject.tag == "")
+//        {
+//
+//        }
+//		if (other.gameObject.tag == "")
+//        {
+//
+//        }
+//		if (other.gameObject.tag == "")
+//        {
+//
+//        }
 		#endregion
     }
 }
