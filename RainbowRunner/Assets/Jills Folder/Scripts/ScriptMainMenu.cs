462e239407e80b8fa1d7a74d7465880b62b78884
JG24Drive45 <JG24Drive45@gmail.com>
2013-10-26 04:41:15 -0400

    The
    dishes are done man!

---
+++ RainbowRunner/Assets/Jills Folder/Scripts/ScriptMainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptMainMenu : MonoBehaviour {
+	
+	private RaycastHit hit = new RaycastHit();
+	private Ray ray = new Ray();
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+			if(Physics.Raycast(ray, out hit, 10.0f))
+			{
+				if(hit.collider.transform.tag == "StartButton")
+				{
+					Debug.Log("Start Clicked");	
+				}
+				else if(hit.collider.transform.tag == "ExitButton")
+				{
+					Debug.Log("Exit Clicked");	
+				}
+			}
+		}
+	}
+
+}
