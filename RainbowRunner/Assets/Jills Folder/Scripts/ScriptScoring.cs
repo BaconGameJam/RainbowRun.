86aca6dd7ff60e430c1e1f5667f2a0d3a93a49ad
JG24Drive45 <JG24Drive45@gmail.com>
2013-10-26 00:25:34 -0400

    GUI
    More GUI

--- RainbowRunner/Assets/Jills Folder/Scripts/ScriptScoring.cs
+++ RainbowRunner/Assets/Jills Folder/Scripts/ScriptScoring.cs
@@ -2,14 +2,42 @@
 using System.Collections;
 
 public class ScriptScoring : MonoBehaviour {
+	
+	public GUIStyle scoreStyle;
+	
+	public int score, distance, coins;
 
 	// Use this for initialization
 	void Start () {
-	
+		score = distance = coins = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		distance += 1;						// Update the distance every frame
+		
+		if(coins <= 0)						// If score is less than 0
+		{
+			score = distance;					// Then only use distance
+		}
+		else
+		{
+			score = distance * coins;			// Total score is distance X coins
+		}
+	}
 	
+	void AddCoin()
+	{
+		coins += 1;	
+	}
+	
+	// GUI FOR SCORING
+	void OnGUI()
+	{
+		GUI.Label(new Rect(10, 10, 100, 100), 
+			"Score: " + score.ToString() + 
+			"\nDistance: " + distance + 
+			"\nCoins: " + coins, 
+			scoreStyle);	
 	}
 }
--- RainbowRunner/Assets/Jills Folder/Scripts/ScriptTime.cs
+++ RainbowRunner/Assets/Jills Folder/Scripts/ScriptTime.cs
@@ -7,27 +7,67 @@ public class ScriptTime : MonoBehaviour {
 	public float timeRemaining;
 	public GUIStyle timeStyle;
 	
+	private bool fontBoost;
+	private float fontBoostTime;
+	private int smallTextOffset, largeTextOffset;
 
 	// Use this for initialization
 	void Start () {
+		smallTextOffset = 100;
+		largeTextOffset = 200;
 		GUI.color = Color.red;
+		fontBoost = false;
+		fontBoostTime = 0.5f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		timeRemaining -= Time.deltaTime;
+		
+		// For testing
+		if(timeRemaining <= 0.0f)
+		{
+			AddTime(10.0f);	
+		}
+		
+		if(fontBoost)
+		{
+			if(fontBoostTime > 0.0f)
+			{
+				fontBoostTime -= Time.deltaTime;	
+			}
+			else
+			{
+				fontBoost = false;
+				timeStyle.fontSize = 50;
+				timeStyle.normal.textColor = Color.red;
+			}
+		}
 	}
 	
 	// DISPLAY THE TIME REMAINING
 	void OnGUI()
 	{
-		GUI.Label(new Rect((Screen.width / 2) - 100, 10, 100, 20), 
-			"Time: " + timeRemaining.ToString("F2"), timeStyle);	
+		if(fontBoost)
+		{
+			GUI.Label(new Rect((Screen.width / 2) - largeTextOffset, 50, 400, 20), 
+				"Time: " + timeRemaining.ToString("F2"), timeStyle);
+		}
+		else
+		{
+			GUI.Label(new Rect((Screen.width / 2) - smallTextOffset, 50, 400, 20), 
+				"Time: " + timeRemaining.ToString("F2"), timeStyle);
+		}
 	}
 	
 	// ADD TIME TO THE REMAINING TIME
 	void AddTime(float plusTime)
 	{
 		timeRemaining += plusTime;
+		timeStyle.fontSize = 100;
+		fontBoost = true;
+		fontBoostTime = 0.5f;
+		timeStyle.normal.textColor = Color.white;
 	}
 }
