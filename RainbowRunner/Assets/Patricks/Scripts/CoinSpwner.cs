43a6500b5bdf9afb53ef9946be4d216710030408
patrick rasmussen <mcman407@gmail.com>
2013-11-04 09:51:25 -0500

    updated coins

---
+++ RainbowRunner/Assets/Patricks/Scripts/CoinSpwner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpwner : MonoBehaviour {
+	public int currentCount, maxCount;
+	public GameObject goldcoin, parentObject;
+	private GameObject [] clones;
+	// Use this for initialization
+	void Start () 	
+	{
+		maxCount = 10;
+		clones = new GameObject[maxCount];
+		for(int i  = 0; currentCount < maxCount; i++)
+		{
+			clones[i] = Instantiate(goldcoin)as GameObject;
+			//clones[i].transform.parent = parentObject;
+			currentCount++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
--- RainbowRunner/Assets/Patricks/Scripts/goldCoinScript.cs
+++ RainbowRunner/Assets/Patricks/Scripts/goldCoinScript.cs
@@ -3,77 +3,91 @@ using System.Collections;
 
 public class goldCoinScript : MonoBehaviour 
 {
-	public GameObject player;
-	private mainPlayer playerScript;
-	public GameObject goldcoin;
-	public float rotSpeed, slowSpeed, fastSpeed, currSpeed, rotpos, 
-	currTime, startTime, startPos;
-	private Vector3 endPos, startP;
-	private int coinCount, maxCoins;
-	public bool goingSlow, goingFast;
-    public int tempStart;
-	// USED TO TRACK THE CURRENT ROW OF THE OBJECT SO PLAYER CANNOT COLLECT IF NOT ON THE SAME ROW
-	public enum ColorState{RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, NONE};
-	public ColorState colorState;
+	public struct goldCoinClone
+	{
+		public Vector3 startPosition, endPosition;
+		public int currentLane, currentCoin;
+		public float currentSpeed, startTime, currentTime, rotSpeed, rotPosition;
+		public bool fast,slow;
+		
+	}
 	
+	public GameObject goldcoin;
+	public goldCoinClone [] goldCoinClones;
+	private GameObject [] clonedCoins;
+	public float  startPos, fastSpeed, slowSpeed, stopSpeed;
+	public int coinCount, maxCoins, tempStart;
+		
 
 	// Use this for initialization
 	void Start () 
 	{
+		maxCoins = 10;
+		goldCoinClones = new goldCoinClone[maxCoins];	
+		clonedCoins = new GameObject[maxCoins];
+		for(int i = 0; coinCount < maxCoins; i++)
+		{
+			goldCoinClones[i].currentCoin = coinCount;
+			goldCoinClones[i].startPosition = new Vector3(30, getStartPos(), -1);
+			goldCoinClones[i].startTime = getStartTime();
+			goldCoinClones[i].fast = true;
+			goldCoinClones[i].slow = false;
+			goldCoinClones[i].currentSpeed = 0.0f;
+			goldCoinClones[i].currentLane = tempStart;
+			goldCoinClones[i].rotSpeed = 0.1f;
+			goldCoinClones[i].currentTime = 0.0f;
+			clonedCoins[i] = Instantiate(goldcoin, goldCoinClones[i].startPosition, Quaternion.identity) as GameObject;
+					
+			coinCount++;
+		}
 		
-		rotSpeed = 0.01f;
+		
 		slowSpeed = 4.0f;
 		fastSpeed = 6.0f;
-		currSpeed = 0.0f;
-		rotpos = 0.0f;
-		startP = new Vector3(30, getStartPos(), -1);
-		endPos = new Vector3(-30,startP.y,-1);
-		this.transform.position = startP;
-		startTime = getStartTime();
-		playerScript = player.GetComponent<mainPlayer>();
+		stopSpeed = 0.0f;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		
-		
-		if(startTime < currTime)
+		for(int i = 0; i < maxCoins; i++)
 		{
-			if(goingSlow)
+			if(goldCoinClones[i].startTime < goldCoinClones[i].currentTime)
 			{
-				currSpeed = slowSpeed;
-			}
-			else if(goingFast)
-			{
-				currSpeed = fastSpeed;
-			}
-			else
-			{
-				currSpeed = 0.0f;
+				if(goldCoinClones[i].fast)
+				{
+					goldCoinClones[i].currentSpeed = fastSpeed;
+				}
+				else if(goldCoinClones[i].slow)
+				{
+					goldCoinClones[i].currentSpeed = slowSpeed;
+				}
+				else
+				{
+					goldCoinClones[i].currentSpeed = 0.0f;
+				}
 				
+				if(goldCoinClones[i].currentSpeed != 0)
+				{
+					if(clonedCoins[i].transform.position.x > -30)
+					{
+						goldCoinClones[i].rotPosition += goldCoinClones[i].rotSpeed;
+						clonedCoins[i].transform.position -= new Vector3(goldCoinClones[i].currentSpeed * Time.deltaTime, 0 , 0);
+						clonedCoins[i].transform.Rotate(0,goldCoinClones[i].rotPosition, 0);
+					}
+					else if(clonedCoins[i].transform.position.x <= -30)
+					{
+						goldCoinClones[i].currentTime = 0;
+						goldCoinClones[i].startPosition = new Vector3(30, getStartPos(), -1);
+						goldCoinClones[i].startTime = getStartTime();
+						clonedCoins[i].transform.position = goldCoinClones[i].startPosition;
+					}
+				}
 			}
+			goldCoinClones[i].currentTime += Time.deltaTime;
 		}
-		if (currSpeed != 0)
-		{
-			this.transform.position -= new Vector3(currSpeed * Time.deltaTime, 0,0);
-		}
-		
-		// If the coin has moved off screen to the left, reset it
-		if( this.transform.position.x <= -30)
-		{
-			currTime = 0;
-			currSpeed = 0;
-			rotpos = 0;
-			getStartTime();
-			SetStartPosition();
-			this.transform.position = startP;
-		}
-		rotpos += rotSpeed;
-		this.transform.Rotate(0, rotpos, 0);
-		//this.transform.localRotation = new Quaternion(0,rotpos,0,0);
-		currTime += Time.deltaTime;
-
 	}
 	
 	// Returns a float to be used for the starting Y position
@@ -85,27 +99,21 @@ public class goldCoinScript : MonoBehaviour
 		{
 		case 0:
 			startPos = 0.4f;	// RED
-			colorState = ColorState.RED;
 			break;
 		case -1:
 			startPos = -0.45f;	// ORANGE
-			colorState = ColorState.ORANGE;
 			break;
 		case -2:
 			startPos = -1.3f;	// YELLOW
-			colorState = ColorState.YELLOW;
 			break;
 		case -3:
 			startPos = -2.2f;	// GREEN
-			colorState = ColorState.GREEN;
 			break;
 		case -4:
 			startPos = -3.0f;	// BLUE
-			colorState = ColorState.BLUE;
 			break;
 		case -5:
 			startPos = -4.0f;	// PURPLE
-			colorState = ColorState.PURPLE;
 			break;
 			
 		}
@@ -119,15 +127,11 @@ public class goldCoinScript : MonoBehaviour
 	}
 	float getStartTime()
 	{
-		currTime = Time.deltaTime;
-		startTime = currTime + randomTime();
+		
+		float startTime = randomTime();
 		return startTime;
 	}
-	
-	void SetStartPosition()
-	{
-		startP = new Vector3(30, getStartPos(), -1);	
-	}
+
 	/*public void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.tag == "Player" && tempStart == playerScript.currentLane)
